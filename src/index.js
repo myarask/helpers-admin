5b2ef05b65ae33394f975ef701c93b63829ffee8
myarask <myaraskavitch@gobonfire.com>
2020-12-08 22:05:08 -0500

    test

--- src/index.js
+++ src/index.js
@@ -78,19 +78,21 @@ const AuthorizedApolloProvider = ({ children }) => {
   return <ApolloProvider client={client}>{children}</ApolloProvider>;
 };
 
-ReactDOM.render(
-  <Auth0Provider
-    domain={process.env.REACT_APP_AUTH_DOMAIN}
-    clientId={process.env.REACT_APP_AUTH_CLIENT_ID}
-    redirectUri={window.location.origin}
-  >
-    <AuthorizedApolloProvider>
-      <CssBaseline />
-      <App />
-    </AuthorizedApolloProvider>
-  </Auth0Provider>,
-  document.getElementById('root')
-);
+console.log(process.env);
+
+// ReactDOM.render(
+//   <Auth0Provider
+//     domain={process.env.REACT_APP_AUTH_DOMAIN}
+//     clientId={process.env.REACT_APP_AUTH_CLIENT_ID}
+//     redirectUri={window.location.origin}
+//   >
+//     <AuthorizedApolloProvider>
+//       <CssBaseline />
+//       <App />
+//     </AuthorizedApolloProvider>
+//   </Auth0Provider>,
+//   document.getElementById('root')
+// );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
